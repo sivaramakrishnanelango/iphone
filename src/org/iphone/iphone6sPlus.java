9633b540f181c591e6b744b17a1d9a7d36bdc018
sivaramakrishnan <sivaram.e98@gmail.com>
2022-08-19 15:10:43 +0530

    new codes are added

---
+++ src/org/iphone/iphone6sPlus.java
@@ -0,0 +1,29 @@
+package org.iphone;
+
+public class iphone6sPlus {
+
+	private void iphone11pro() {
+		System.out.println("price is 64000");
+
+	}
+	private void iphone10() {
+		System.out.println("price is 75000");
+
+	}
+	private void iphone13pro() {
+		System.out.println("price is 85000");
+
+	}
+	private void iphone13promax() {
+		System.out.println("price is 100000");
+
+	}
+	public static void main(String[] args) {
+		
+		iphone6sPlus i = new iphone6sPlus();
+		i.iphone10();
+		i.iphone11pro();
+		i.iphone13pro();
+		i.iphone13promax();
+	}
+}
